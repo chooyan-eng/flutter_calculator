34bd2882381b007ad3f257ed0b8e47631577a0b0
tsuyoshi-chujo <chooyan.engineer@gmail.com>
2021-08-05 11:26:44 +0900

    part 2

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,52 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+enum Operator { plus, minus, time, divide }
+
 class _MyHomePageState extends State<MyHomePage> {
+  final _panelActions = <String, VoidCallback>{};
+
+  @override
+  void initState() {
+    _panelActions.addAll({
+      '1': () => _inputNumber(1),
+      '2': () => _inputNumber(2),
+      '3': () => _inputNumber(3),
+      '4': () => _inputNumber(4),
+      '5': () => _inputNumber(5),
+      '6': () => _inputNumber(6),
+      '7': () => _inputNumber(7),
+      '8': () => _inputNumber(8),
+      '9': () => _inputNumber(9),
+      '0': () => _inputZero(),
+      '00': () => _inputZero(digits: 2),
+      'AC': _clear,
+      '+/-': _toggleSign,
+      '%': _percentize,
+      '.': _inputDot,
+      '+': () => _inputOperator(Operator.plus),
+      '-': () => _inputOperator(Operator.minus),
+      '*': () => _inputOperator(Operator.time),
+      '/': () => _inputOperator(Operator.divide),
+      '=': () => _inputOperator(Operator.divide),
+    });
+    super.initState();
+  }
+
+  void _inputNumber(int number) {}
+
+  void _inputZero({int digits = 1}) {}
+
+  void _clear() {}
+
+  void _toggleSign() {}
+
+  void _percentize() {}
+
+  void _inputDot() {}
+
+  void _inputOperator(Operator opr) {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,38 +78,33 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Expanded(
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                color: Colors.black12,
+                margin: const EdgeInsets.all(1),
+                padding: const EdgeInsets.all(8),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      '10 + 1000',
+                      style: const TextStyle(fontSize: 20),
+                    ),
+                    Text(
+                      '1010',
+                      style: const TextStyle(fontSize: 40),
+                    ),
+                  ],
+                ),
+              ),
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 const SizedBox(width: 1),
-                Expanded(
-                  child: Panel(
-                    label: 'AC',
-                    onTap: () {},
-                  ),
-                ),
-                const SizedBox(width: 1),
-                Expanded(
-                  child: Panel(
-                    label: '+/-',
-                    onTap: () {},
-                  ),
-                ),
-                const SizedBox(width: 1),
-                Expanded(
-                  child: Panel(
-                    label: '%',
-                    onTap: () {},
-                  ),
-                ),
-                const SizedBox(width: 1),
-                Expanded(
-                  child: Panel(
-                    label: '/',
-                    onTap: () {},
-                  ),
-                ),
-                const SizedBox(width: 1),
+                ..._buildPanelsRow(['AC', '+/-', '%', '/'])
               ],
             ),
             const SizedBox(height: 1),
@@ -72,34 +112,7 @@ class _MyHomePageState extends State<MyHomePage> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 const SizedBox(width: 1),
-                Expanded(
-                  child: Panel(
-                    label: '7',
-                    onTap: () {},
-                  ),
-                ),
-                const SizedBox(width: 1),
-                Expanded(
-                  child: Panel(
-                    label: '8',
-                    onTap: () {},
-                  ),
-                ),
-                const SizedBox(width: 1),
-                Expanded(
-                  child: Panel(
-                    label: '9',
-                    onTap: () {},
-                  ),
-                ),
-                const SizedBox(width: 1),
-                Expanded(
-                  child: Panel(
-                    label: '*',
-                    onTap: () {},
-                  ),
-                ),
-                const SizedBox(width: 1),
+                ..._buildPanelsRow(['7', '8', '9', '*'])
               ],
             ),
             const SizedBox(height: 1),
@@ -107,34 +120,7 @@ class _MyHomePageState extends State<MyHomePage> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 const SizedBox(width: 1),
-                Expanded(
-                  child: Panel(
-                    label: '4',
-                    onTap: () {},
-                  ),
-                ),
-                const SizedBox(width: 1),
-                Expanded(
-                  child: Panel(
-                    label: '5',
-                    onTap: () {},
-                  ),
-                ),
-                const SizedBox(width: 1),
-                Expanded(
-                  child: Panel(
-                    label: '6',
-                    onTap: () {},
-                  ),
-                ),
-                const SizedBox(width: 1),
-                Expanded(
-                  child: Panel(
-                    label: '-',
-                    onTap: () {},
-                  ),
-                ),
-                const SizedBox(width: 1),
+                ..._buildPanelsRow(['4', '5', '6', '-'])
               ],
             ),
             const SizedBox(height: 1),
@@ -142,34 +128,7 @@ class _MyHomePageState extends State<MyHomePage> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 const SizedBox(width: 1),
-                Expanded(
-                  child: Panel(
-                    label: '1',
-                    onTap: () {},
-                  ),
-                ),
-                const SizedBox(width: 1),
-                Expanded(
-                  child: Panel(
-                    label: '2',
-                    onTap: () {},
-                  ),
-                ),
-                const SizedBox(width: 1),
-                Expanded(
-                  child: Panel(
-                    label: '3',
-                    onTap: () {},
-                  ),
-                ),
-                const SizedBox(width: 1),
-                Expanded(
-                  child: Panel(
-                    label: '+',
-                    onTap: () {},
-                  ),
-                ),
-                const SizedBox(width: 1),
+                ..._buildPanelsRow(['1', '2', '3', '+'])
               ],
             ),
             const SizedBox(height: 1),
@@ -177,40 +136,25 @@ class _MyHomePageState extends State<MyHomePage> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 const SizedBox(width: 1),
-                Expanded(
-                  child: Panel(
-                    label: '0',
-                    onTap: () {},
-                  ),
-                ),
-                const SizedBox(width: 1),
-                Expanded(
-                  child: Panel(
-                    label: '00',
-                    onTap: () {},
-                  ),
-                ),
-                const SizedBox(width: 1),
-                Expanded(
-                  child: Panel(
-                    label: '.',
-                    onTap: () {},
-                  ),
-                ),
-                const SizedBox(width: 1),
-                Expanded(
-                  child: Panel(
-                    label: '=',
-                    onTap: () {},
-                  ),
-                ),
-                const SizedBox(width: 1),
+                ..._buildPanelsRow(['0', '00', '.', '='])
               ],
             ),
           ],
         ),
       ),
     );
+  }
+
+  List<Widget> _buildPanelsRow(List<String> labels) {
+    return labels
+        .map((label) => Expanded(
+              child: Panel(
+                label: label,
+                onTap: _panelActions[label]!,
+              ),
+            ))
+        .expand((widget) => [widget, const SizedBox(width: 1)])
+        .toList();
   }
 }
 
